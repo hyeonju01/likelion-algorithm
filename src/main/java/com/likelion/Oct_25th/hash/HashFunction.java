a7b7743604e261b6a14e2de0f8140a9b3e60c47a
hyeonju01 <tangled6648@gmail.com>
2022-10-25 09:12:03 +0900

    22. 10. 25. Hash 구현 1. 처음 구조 잡기

---
+++ src/main/java/com/likelion/Oct_25th/hash/HashFunction.java
@@ -0,0 +1,10 @@
+package com.likelion.Oct_25th.hash;
+
+public class HashFunction {
+    public int hash(String key) {
+        return 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
