98024515a27f2faff38e257d666f915cc1b977b9
hyeonju01 <tangled6648@gmail.com>
2022-10-28 10:49:40 +0900

    22. 10. 27 [Programmers] 전화번호 목록 - 에러 해결중 - HashSet 사용 풀이 - 프로그래머스 코드 실행 error

--- src/main/java/com/likelion/Oct_28th/PhoneNumBook.java
+++ src/main/java/com/likelion/Oct_28th/PhoneNumBook.java
@@ -16,24 +16,30 @@ public class PhoneNumBook {
         String prefix = ""; // 접두어 변수 초기화
         int prefixIdx = 0; // 길이가 가장 짧은 접두어의 인덱스 변수 초기화
         for (int i = 0; i<phone_book.length; i++) {
-            int phoneNumLen = phone_book[i].length();
+            int phoneNumLen = phone_book[i].length(); //i번째 전화번호의 길이
             for (int j = 1; j<phone_book.length; j++) {
-                if (phoneNumLen > phone_book[j].length())
+                if (phoneNumLen > phone_book[j].length()) {
                     phoneNumLen = phone_book[j].length();
-                prefixIdx = j;
+                    prefixIdx = j;
+                } else {
+                    prefixIdx = i;
+                }
             }
         }
-        prefix = phone_book[prefixIdx];
 
+        prefix = phone_book[prefixIdx]; //가장 짧은 접두어
+
+        //phone_book 배열 요소를 hashSet에 넣기
         for ( String phoneNum : phone_book) {
             hashSet.add(phoneNum);
         }
 
+        // hashSet 요소 순회하며 가장 짧은 접두어 인덱스가 있는지 확인
         for (String num : hashSet) {
-            if ((num.indexOf(prefix)) !=0) {
-                return false;
+            if ((num.indexOf(prefix)) > -1) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
