a457211b90b1ca84d24dde40c0f461eb4ba48dd5
hyeonju01 <tangled6648@gmail.com>
2022-11-01 10:46:22 +0900

    22. 11. 1 소수 찾기 - 템플릿 콜백 패턴 실습(인터페이스, 람다)

--- src/main/java/com/likelion/Nov_1st/TemplateCallbackPrime.java
+++ src/main/java/com/likelion/Nov_1st/TemplateCallbackPrime.java
@@ -33,8 +33,11 @@ public class TemplateCallbackPrime {
         return true;
     }
 
+    // lambda 사용
     public static void main(String[] args) {
         TemplateCallbackPrime tcp = new TemplateCallbackPrime();
-        System.out.println(tcp.isPrime(13));
+        System.out.println(tcp.isPrime(13, (a, b) -> a<b));
+        System.out.println(tcp.isPrime(17, (a, b) -> a<b/2));
+        System.out.println(tcp.isPrime(19, (a, b) -> a*a <= b));
     }
 }
