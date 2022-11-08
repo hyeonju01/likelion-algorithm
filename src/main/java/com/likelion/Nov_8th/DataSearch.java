3c34306010a974a2d509597eafd46f4f623916d4
hyeonju01 <tangled6648@gmail.com>
2022-11-08 09:46:56 +0900

    CodeUp 3001: 데이터 탐색 (에러 수정 완료)

--- src/main/java/com/likelion/Nov_8th/DataSearch.java
+++ src/main/java/com/likelion/Nov_8th/DataSearch.java
@@ -1,23 +1,31 @@
 package com.likelion.Nov_8th;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
-
+import java.util.StringTokenizer;
+/*
+CodeUp 3001: 데이터 탐색 (에러 수정 완료)
+ */
 public class DataSearch {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         int[] numbers = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            numbers[i] = sc.nextInt();
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(numbers);
-        int k = sc.nextInt();
+        System.out.println(Arrays.toString(numbers));
+        int k = Integer.parseInt(br.readLine());
         int answer = -1;
+
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == k) {
-                answer = i;
+                answer = i+1;
             }
         }
         System.out.println(answer);
