9bf458069303abeba2e9777a9b8d6697f9a5884d
hyeonju01 <tangled6648@gmail.com>
2022-11-08 09:34:18 +0900

    CodeUp 3001: 데이터 탐색(테스트 케이스 틀림)

--- src/main/java/com/likelion/Nov_8th/DataSearch.java
+++ src/main/java/com/likelion/Nov_8th/DataSearch.java
@@ -1,31 +1,25 @@
 package com.likelion.Nov_8th;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class DataSearch {
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); //n개의 데이터 입력 받기
-        int[] arr = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = sc.nextInt();
         }
-
-        Arrays.sort(arr);
-
-        int k = Integer.parseInt(br.readLine()); // 목표 수 k
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == k) {
-                System.out.println(i);;
+        Arrays.sort(numbers);
+        int k = sc.nextInt();
+        int answer = -1;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == k) {
+                answer = i;
             }
-            System.out.println(-1);
         }
+        System.out.println(answer);
     }
 }
