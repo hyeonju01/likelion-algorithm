3bf0ca63ba198121f857c8dea7faa6f5e2d591e5
hyeonju01 <tangled6648@gmail.com>
2022-11-08 10:46:24 +0900

    이진탐색 구현 (성!공!)

--- src/main/java/com/likelion/Nov_8th/BinarySearch.java
+++ src/main/java/com/likelion/Nov_8th/BinarySearch.java
@@ -3,21 +3,24 @@ package com.likelion.Nov_8th;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        //중간값 찾기
+        //1.중간값 찾기
         int start = 0;
-        int end = nums.length-1;
-        int mid = (start+end)/2;
+        int end = nums.length - 1;
+        int mid = (start + end)/2;
 
         int target = 7;
         int answer = 0;
+
         while (start <= end) {
-            if(nums[mid] == target) {
+            if (nums[mid] == target) {
                 answer = mid;
                 break;
             } else if (nums[mid] < target) {
                 start = mid + 1;
+                continue;
             } else {
                 end = mid -1;
+                continue;
             }
         }
         System.out.println(answer);
