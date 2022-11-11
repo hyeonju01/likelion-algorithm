89c144976685afbd053d9de3bc06fdeaa79dc188
hyeonju01 <tangled6648@gmail.com>
2022-11-11 09:16:49 +0900

    [Programmers] 비밀 지도 문제 풀이 idea

---
+++ src/main/java/com/likelion/Nov_11st/SecretMap.java
@@ -0,0 +1,14 @@
+package com.likelion.Nov_11st;
+
+/*
+[kakao] 비밀 지도
+https://school.programmers.co.kr/learn/courses/30/lessons/17681
+ */
+public class SecretMap {
+    /*
+    [idea]
+    1. 정수 배열로 주어진 arr1, arr2를 이진수 배열로 변환한다.
+    2. 두 배열의 동일한 인덱스에 있는 이진수끼리 더한 answer 배열을 구한다.
+    3. answer 배열의 이진수에 따라 숫자가 1이면 #을, 0이면 공백을 출력하는 secretMap 배열을 반환한다.
+     */
+}
