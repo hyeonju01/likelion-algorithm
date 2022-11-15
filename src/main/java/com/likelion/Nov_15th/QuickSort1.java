9c9fd59d8d072cc6bc2102e2c5af215d5ee59497
hyeonju01 <tangled6648@gmail.com>
2022-11-15 10:36:37 +0900

    feat: 퀵 정렬 구현 2단계- 메소드 분리(merge, sort)- error(OutOfMemoryError)

--- src/main/java/com/likelion/Nov_15th/QuickSort1.java
+++ src/main/java/com/likelion/Nov_15th/QuickSort1.java
@@ -6,39 +6,52 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort1 {
-    public static void main(String[] args) {
-        var arr = new int[] {20, 18, 5, 19, 5, 25, 40, 50};
+
+    public List<Integer> merge(List<Integer> left,
+                               List<Integer> mid,
+                               List<Integer> right){
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(mid);
+        answer.addAll(right);
+
+        return answer;
+    }
+
+    public List<Integer> sort(List<Integer> arr) {
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
         List<Integer> mid = new ArrayList<>();
-        int midnum = 0;
-        midnum = arr[arr.length/2];
-        mid.add(midnum);
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > midnum) {
-                right.add(arr[i]);
-            } else {
-                left.add(arr[i]);
+        int pivot = arr.get(arr.size()/2);
+        mid.add(pivot);
+        while (arr.size() > 1) {
+            for (int i = 0; i < arr.size(); i++) {
+                if ( pivot < arr.get(i)) {
+                    right.add(arr.get(i));
+                } else if (pivot >= arr.get(i)){
+                    left.add(arr.get(i));
+                } else {
+                    mid.add(arr.get(i));
+                }
             }
         }
+        return merge(sort(left), sort(mid), sort(right));
+    }
 
-        int[] leftArr = new int[left.size()];
-        int[] rightArr = new int[right.size()];
-        int[] midArr = new int[mid.size()];
-
-        for (int i = 0; i < left.size(); i++) {
-            leftArr[i] = left.get(i).intValue();
-        }
-        for (int i = 0; i < right.size(); i++) {
-            rightArr[i] = right.get(i).intValue();
-        }
-        for (int i = 0; i < mid.size(); i++) {
-            midArr[i] = mid.get(i).intValue();
+    public static void main(String[] args) {
+        var arr = new int[] {20, 18, 5, 19, 5, 25, 40, 50}; //size=8
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < arr.length ; i++) {
+            al.add(arr[i]);
         }
 
-        System.out.println("원래 정렬은: " + Arrays.toString(arr));
-        System.out.println("pivot: " + Arrays.toString(midArr));
-        System.out.println("피봇 왼쪽 정렬은: " + Arrays.toString(leftArr));
-        System.out.println("피봇 오른쪽 정렬은: " + Arrays.toString(rightArr));
+        /*
+        [코드를 보는 훈련] -> 뇌를 컴파일러로 만드는 방법!!~!! *^^*
+        바로 실행하지 않고 예상 출력값을 생각해보면 좋다.
+        내가 생각한대로 로직이 실행되었는지 확인할 수 있기 때문이다.
+         */
+
+        QuickSort1 qs = new QuickSort1();
+        qs.sort(al);
     }
 }
