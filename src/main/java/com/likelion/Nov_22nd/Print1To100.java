b4af2d11b69e5bfbe6813da8660f424ddb7cd161
hyeonju01 <tangled6648@gmail.com>
2022-11-22 09:20:12 +0900

    feat: 재귀 호출 해보기 - 탈출 조건 넣기

--- src/main/java/com/likelion/Nov_22nd/Print1To100.java
+++ src/main/java/com/likelion/Nov_22nd/Print1To100.java
@@ -3,7 +3,10 @@ package com.likelion.Nov_22nd;
 public class Print1To100 {
     public static void print1To100(int n){
         System.out.println(n);
-        print1To100(n);
+        n--;
+        if (n > 0) {
+            print1To100(n);
+        }
     }
 
     public static void main(String[] args) {
