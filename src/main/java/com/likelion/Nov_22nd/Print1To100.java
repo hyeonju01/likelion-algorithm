9ec795c672ac8ff0ad2c9c3b70e03bec463caffe
hyeonju01 <tangled6648@gmail.com>
2022-11-22 09:26:07 +0900

    feat: 재귀 호출 해보기 - 1씩 커지는 로직 추가

--- src/main/java/com/likelion/Nov_22nd/Print1To100.java
+++ src/main/java/com/likelion/Nov_22nd/Print1To100.java
@@ -2,11 +2,12 @@ package com.likelion.Nov_22nd;
 
 public class Print1To100 {
     public static void print1To100(int n){
-        System.out.println(n);
-        n--;
-        if (n > 0) {
-            print1To100(n);
+        if (n > 100) {
+            return;
         }
+        System.out.println(n);
+        n++;
+        print1To100(n);
     }
 
     public static void main(String[] args) {
