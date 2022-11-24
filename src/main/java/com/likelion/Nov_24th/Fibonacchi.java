4115c05a95d5d48ace2ecda199d924b32418328a
hyeonju01 <tangled6648@gmail.com>
2022-11-24 09:40:15 +0900

    feat: 피보나치 수열- 재귀 사용, codeup-1855

--- src/main/java/com/likelion/Nov_24th/Fibonacchi.java
+++ src/main/java/com/likelion/Nov_24th/Fibonacchi.java
@@ -2,7 +2,10 @@ package com.likelion.Nov_24th;
 
 /*
 재귀를 사용하여 피보나치 수열 구현하기
+[코드업 1855] [기초-재귀함수] 재귀로 n번째 피보나치 수 리턴하기
  */
+
+import java.util.Scanner;
 
 public class Fibonacchi {
 
@@ -18,8 +21,9 @@ public class Fibonacchi {
     }
 
     public static void main(String[] args) {
-        int r = fib(6);
-        System.out.println(r);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fib(n));
 
     }
 }
